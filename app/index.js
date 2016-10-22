6e679fbb997c1e416e2832a1ca4572885eb28244
Nick Martinez <23nicholasmartinez@gmail.com>
2016-10-22 16:24:09 -0700

    Add first component and local server

--- app/index.js
+++ app/index.js
@@ -1,2 +1,15 @@
-var app = document.getElementById('app');
-app.innerHTML = 'Hello'
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var HelloWoyld = React.createClass({
+  render: function () {
+    return (
+      <div> Hello Woyld </div>
+    )
+  }
+});
+
+ReactDOM.render(
+  <HelloWoyld />,
+  document.getElementById('app')
+);
