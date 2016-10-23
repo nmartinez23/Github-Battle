e45869785615013836c6b93b34b16e16dcb8b5ea
Nick Martinez <23nicholasmartinez@gmail.com>
2016-10-23 10:35:52 -0700

    Create Main and Home components

--- app/index.js
+++ app/index.js
@@ -1,66 +1,8 @@
-var USER_DATA = {
-  name: 'Nick',
-  username: 'nmartinez23',
-  image: 'http://i.imgur.com/HMANO96.jpg'
-}
-
 var React = require('react');
 var ReactDOM = require('react-dom');
-
-var ProfilePic = React.createClass({
-  render: function () {
-    return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
-  }
-});
-
-var Link = React.createClass({
-  changeURL: function () {
-    window.location.replace(this.props.href)
-  },
-  render: function () {
-    return (
-      <span
-        style={{color: 'blue', cursor: 'pointer'}}
-        onClick={this.changeURL}>
-        {this.props.children}
-      </span>
-    )
-  }
-})
-
-var ProfileLink = React.createClass({
-  render: function () {
-    return (
-      <div>
-        <Link href={'https://www.github.com/' + this.props.username}>
-          {this.props.username}
-        </Link>
-      </div>
-    )
-  }
-});
-
-var ProfileName = React.createClass({
-  render: function () {
-    return (
-      <div>{this.props.name}</div>
-    )
-  }
-});
-
-var Avatar = React.createClass({
-  render: function () {
-    return (
-      <div>
-        <ProfilePic imageUrl={this.props.user.image} />
-        <ProfileName name={this.props.user.name} />
-        <ProfileLink username={this.props.user.username} />
-      </div>
-    )
-  }
-})
+var routes = require('./config/routes');
 
 ReactDOM.render(
-  <Avatar user={USER_DATA} />,
+  routes,
   document.getElementById('app')
 );
