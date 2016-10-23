82ade159fb67bf28a1808a753f67a3347c54caa0
Nick Martinez <23nicholasmartinez@gmail.com>
2016-10-23 12:22:47 -0700

    Add Sentry error tracking

--- app/index.js
+++ app/index.js
@@ -1,8 +1,23 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var routes = require('./config/routes');
+var Raven = require('raven-js');
 
-ReactDOM.render(
-  routes,
-  document.getElementById('app')
-);
+var sentryKey = '798b5c2cd8c3419f9324183063373a90'
+var sentryApp = '108573'
+var sentryURL = 'https://' + sentryKey + '@app.getsentry.com/' + sentryApp
+
+var _APP_INFO = {
+  name: 'Github Battle',
+  branch: 'video4',
+  version: '1.0'
+}
+
+Raven.config(sentryURL, {
+  release: _APP_INFO.version,
+  tags: {
+    branch: _APP_INFO.branch,
+  }
+}).install()
+
+ReactDOM.render(routes, document.getElementById('app'));
