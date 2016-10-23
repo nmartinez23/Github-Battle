d37e7993a8440e1e74d49c15ec364c1378cfe21f
Nick Martinez <23nicholasmartinez@gmail.com>
2016-10-23 09:24:23 -0700

    Add user profile components

--- app/index.js
+++ app/index.js
@@ -1,15 +1,51 @@
+var USER_DATA = {
+  name: 'Nick',
+  username: 'nmartinez23',
+  image: 'http://i.imgur.com/HMANO96.jpg'
+}
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWoyld = React.createClass({
+var ProfilePic = React.createClass({
+  render: function () {
+    return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
+  }
+});
+
+var ProfileLink = React.createClass({
   render: function () {
     return (
-      <div> Hello Woyld </div>
+      <div>
+        <a href={'https://www.github.com/' + this.props.username}>
+          {this.props.username}
+        </a>
+      </div>
     )
   }
 });
 
+var ProfileName = React.createClass({
+  render: function () {
+    return (
+      <div>{this.props.name}</div>
+    )
+  }
+});
+
+var Avatar = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <ProfilePic imageUrl={this.props.user.image} />
+        <ProfileName name={this.props.user.name} />
+        <ProfileLink username={this.props.user.username} />
+      </div>
+    )
+  }
+})
+
 ReactDOM.render(
-  <HelloWoyld />,
+  <Avatar user={USER_DATA} />,
   document.getElementById('app')
 );
