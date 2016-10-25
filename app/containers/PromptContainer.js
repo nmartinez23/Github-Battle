ecf204d86423a08c68616237ae0afab743d72b17
Nick Martinez <23nicholasmartinez@gmail.com>
2016-10-25 09:04:59 -0700

    Fix: Module exports now included

--- app/containers/PromptContainer.js
+++ app/containers/PromptContainer.js
@@ -10,11 +10,6 @@ var PromptContainer = React.createClass({
       username: ''
     }
   },
-  handleUpdateUser: function (e) {
-    this.setState({
-      username: e.target.value
-    })
-  },
   handleSubmitUser: function (e) {
     e.preventDefault();
     var username = this.state.username;
@@ -27,12 +22,17 @@ var PromptContainer = React.createClass({
         pathname: '/battle',
         query: {
           playerOne: this.props.routeParams.playerOne,
-          playerTwo: this.state.username
+          playerTwo: this.state.username,
         }
       })
     } else {
       this.context.router.push('/playerTwo/' + this.state.username)
     }
+  },
+    handleUpdateUser: function (e) {
+    this.setState({
+      username: e.target.value
+    });
   },
   render: function () {
     return (
@@ -44,3 +44,5 @@ var PromptContainer = React.createClass({
     )
   }
 });
+
+module.exports = PromptContainer;
