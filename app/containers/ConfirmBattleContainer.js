879a4fbcf4a962897008ff69ae86b72a18e35f65
Nick Martinez <23nicholasmartinez@gmail.com>
2016-10-25 09:08:47 -0700

    Add ConfirmBattle components for UI and logic

---
+++ app/containers/ConfirmBattleContainer.js
@@ -0,0 +1,13 @@
+var React = require('react');
+var ConfirmBattle = require('../components/ConfirmBattle');
+
+var ConfirmBattleContainer = React.createClass({
+  render: function() {
+    return (
+      <ConfirmBattle />
+    );
+  }
+
+});
+
+module.exports = ConfirmBattleContainer;
