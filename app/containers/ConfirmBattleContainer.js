6e6ac01d437c78bb06cacc817321195968a8d142
Nick Martinez <23nicholasmartinez@gmail.com>
2016-10-25 17:07:33 -0700

    Bind this to setState

--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -20,7 +20,7 @@ var ConfirmBattleContainer = React.createClass({
           isLoading: false,
           playersInfo: [players[0], players[1]]
         })
-      })
+      }.bind(this))
   },
   render: function() {
     return (
